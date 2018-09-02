d7b9330370d432003ac731b2b5a9a747bf2ac92e
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-02 02:12:10 -0500

    this is my indepth learning

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const EventEmitter = require('events');
 // caps for both shows that is  a class/
 
+const emitter = new EventEmitter();
+
+//Register a listner
+emitter.on('wow3', function(){
+ console.log('Treek keep moving forward');
+});
+
+
+// Raise an event.
+emitter.emit('wow3');
+
+// Emit making a noise, or produce something. signalling something happened/
