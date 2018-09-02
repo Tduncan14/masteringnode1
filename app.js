60e1f931d848fa34d3d7a8d0ad02c3ca3b095536
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-02 01:21:04 -0500

    more learning

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const os = require('os');
 var totalMemory = os.totalmem();
 var freeMemory = os.freemem();
 
-console.log('total :' + totalyMemory);
+console.log('total :' + totalMemory);
 
 // a Template string
 // ES6 / ES2015 : ECMAScript 6
