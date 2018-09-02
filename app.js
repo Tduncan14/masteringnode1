00260436c26d114a00deca082b795a96d8f351f0
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-01 22:43:14 -0500

    this is a file i am pushing module

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// to load logger module
+
+
+
 console.log(module);
 
 // in node every file is module that needed to export the variables and files found in this module is only in the scope of this module
