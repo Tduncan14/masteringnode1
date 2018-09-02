66adf1157f23dcac07bb4ae18bcbb571e68e51b3
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-02 01:55:25 -0500

    more learning

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-// grabbing the module and using it
-// now i am able to use the methods
-const fs= fileSystem = require('fs');
+const EventEmitter = require('events');
+// caps for both shows that is  a class/
 
-const files =fs.readdirSync('./');
-
-console.log(files);
-
-fs.readdir('./', function(err,files){
-    if(err){
-        console.log('Error',err);
-    }
-    else{
-        console.log('Result : ',files);
-    }
-});
