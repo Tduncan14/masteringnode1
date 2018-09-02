76505c9e285d5d573f74d152dfa741aee7efbbbc
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-01 22:39:33 -0500

    this is a file i am pushing module

--- logger.js
+++ logger.js
@@ -8,3 +8,9 @@ function log(message){
 
 
 // exporting this file.. making a private module go public with another export
+   
+// exports the function out of the module
+module.exports.log = log;
+
+// you can also export the variable outside of the moduel
+module.exports.url = url;
