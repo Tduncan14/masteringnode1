effc30ca2f0f02ecc69b40e44e156c75489900bb
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-01 22:37:21 -0500

    this is a file i am pushing module

---
+++ logger.js
@@ -0,0 +1,10 @@
+
+var url ="http://mylogger.io/log";
+
+function log(message){
+    // send an Http request
+    console.log(message);
+}
+
+
+// exporting this file.. making a private module go public with another export
