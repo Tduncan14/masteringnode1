d824f657049a01c64970ae720dbcfc5ae4502ecf
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-02 22:38:56 -0500

    building a snake game

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 //Register a listner
-emitter.on('wow3', function(arg){
+emitter.on('wow3', (arg) =>{
  console.log('Treek keep moving forward', arg);
 });
 
@@ -13,3 +13,7 @@ emitter.on('wow3', function(arg){
 emitter.emit('wow3',{id:1, url:'http://'});
 
 // Emit making a noise, or produce something. signalling something happened/
+
+
+// Raise an event
+emitter.emit('new stuff',{data:"this is information"});
