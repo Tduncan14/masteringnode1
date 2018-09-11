8d87328b990c9587ea6c5a25bb4e567a331c8955
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-10 19:39:20 -0500

    how node is anyschronous file

--- hiNode/hii.js
+++ hiNode/hii.js
@@ -4,6 +4,17 @@ var fs = require('fs');
 
 // create http server
 http.createServer(function(req,res){
-    // open and read in hellowworld.js
-    fs.readFile('hi.js')
-})
+    // when she is hard headed but you love her
+    fs.readFile('doc.js','utf8', function(err, data){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+      if(err)
+        res.write('Could not find or open file for reading\n');
+      else
+          // if no error, write Js file to client
+          res.write(data);
+          res.end();
+
+    });
+}).listen(8123,function(){console.log('bound to port 8123');});
+
+console.log('Server running on 8123');
