cf0bd61d46c4947f32957ae9413395fb4ec12eaa
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-13 19:11:44 -0500

    this is a message to keep moving forward

--- hiNode/setTimer.js
+++ hiNode/setTimer.js
@@ -5,7 +5,16 @@ function on_OpenAndReadFile(filename,res){
     // open and reads the file name method 
     fs.readFile(filename,'utf-8', function(err,data){
         if(err)
-        res.write('Could not find or open file for reading \n')
+        res.write('Could not find or open file for reading \n');
 
+        else {
+            // if the first condition is not true the data will be passed and open
+            res.write(data);
+        }
+        // once the respons is done, it tries to make a connection
+        
     });
+    res.end();
 }
+
+setTimeout(openAndReadFile,2000, filename, res);
