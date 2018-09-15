3a0ff678ee52eae6d998b2598f5c0c0981972e1b
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-15 12:51:08 -0500

    you will continue to write messages

--- hiNode/readline.js
+++ hiNode/readline.js
@@ -1,1 +1,11 @@
 // readline.js
+// creates a new interface
+
+var interface = readline.createInterface(process.stdin, process.stdout, null);
+
+// ask question
+interface.question( ">> What is the meaning of life? ", function(answer){
+    console.log("what is the meaning of life?");
+    interface.setPrompt(">>");
+    interface.prompt();
+});
