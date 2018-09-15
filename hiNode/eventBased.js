56e68758838e9f1a314108fa0a4fdf6b5a0668b3
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-15 14:33:14 -0500

    still understand the deep methods then i will be creating projects

---
+++ hiNode/eventBased.js
@@ -0,0 +1,4 @@
+// loading modules
+var util = require('util');
+var evntEmitter = require('events'). EventEmitterl
+// file system , which allows you to  write and open files 
--- hiNode/nodeEvent.js
+++ hiNode/nodeEvent.js
@@ -6,3 +6,5 @@ var events = require('events');
 // create an instance eventEmitter
 
 var em = new events.EventEmitter();
+// two essential task attach an event handler to an even, and emit the actual event. The event hadnler is triggered when a specific event is emittedS
+// the first parameter to the method is the name of the event, the second a function to process the event
